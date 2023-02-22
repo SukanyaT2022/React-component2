1d0d9ac6a85f60212ea5c3dc5710fa827cf1691f
TiparpronS <Sukanyat2022a@gmail.com>
2023-02-22 09:26:55 -0500

    add

---
+++ my-app/src/app2.js
@@ -0,0 +1,31 @@
+import logo from './logo.svg';
+import './App.css';
+
+function AppTwo() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+        Dog + Summer !
+        </p>
+        <a
+          className="App-link"
+          href="https://images.unsplash.com/photo-1591160690555-5debfba289f0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8cHVwcHl8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+
+
+
+
+
+    
+  );
+}
+
+export default AppTwo;
---
+++ my-app/src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import AppOne from './app1';
+import AppTwo from './app2';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <AppOne/>
+    <App/>
+    <AppTwo/>
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
